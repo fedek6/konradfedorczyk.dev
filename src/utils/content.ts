0ef03eef5e19d53dc24627e8f9c7bd13f6eb3830
Konrad Fedorczyk <fedek6@gmail.com>
2026-09-03 14:01:26 +0200

    feat: support for mdx content rendering

--- src/utils/content.ts
+++ src/utils/content.ts
@@ -5,3 +5,10 @@ export const createCollectionHeadMapper =
       .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
       .slice(0, itemsCount);
   };
+
+export const groupByYear = <T extends { data: { pubDate: Date } }>(
+  content: T[],
+) =>
+  Object.entries(
+    Object.groupBy(content, (item) => item.data.pubDate.getFullYear()),
+  ).sort(([a], [b]) => +b - +a);
